c7ccdb6d74dce52985fc7fc5663701e7a62bd995
nicolasgodineau <nicolasgodineau@live.fr>
2023-11-01 11:48:39 -0400

    Modification des noms d'images car paroblème d'affichage ajuste du css pour l'affichage des images modification des bg

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Container, Typography } from "@mui/material";
+import { Box, Container, Typography } from "@mui/material";
 import Header from "../Components/header";
 import Image from "next/image";
 
@@ -8,24 +8,33 @@ import path from "path";
 
 function portfolio() {
     const imageDirectory = "/images/portfolio/projet1/";
-    const images = fs.readdirSync(
-        path.join(process.cwd(), "public", imageDirectory)
-    );
+    const images = fs
+        .readdirSync(path.join(process.cwd(), "public", imageDirectory))
+        .slice(1);
+    console.log("images:", images);
 
     return (
         <Container maxWidth="xl" sx={{}}>
             <Header />
-            {images.map((image, index) => (
-                <div key={index}>
+            <Box
+                sx={{
+                    display: "flex",
+                    flexDirection: "column",
+                    alignItems: "center",
+                    paddingTop: "5rem",
+                }}
+            >
+                {images.map((image, index) => (
                     <Image
+                        key={index + 1}
                         src={`${imageDirectory}${image}`}
-                        alt={`Image ${index}`}
+                        alt={`Image ${index + 1}`}
                         width={2000}
                         height={1125}
-                        style={{ maxWidth: "100%", height: "auto" }}
+                        style={{ maxWidth: "80%", height: "auto" }}
                     />
-                </div>
-            ))}
+                ))}
+            </Box>
         </Container>
     );
 }
